b97ed2c377da64c876e7b24154078398692584bf
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-30 14:57:41 +0100

    Created bin/school_repo.py

---
+++ bin/school_repo.py
@@ -0,0 +1,19 @@
+"""school_repo
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-30"
+
+class school_repo:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(school_repo.school_repo())
+	
+	def school_repo():
+		"""school_repo first funtion
+		"""
+		return "school_repo" #for the test (see test_school_repo.py)
+		
+if __name__ == "__main__":
+	school_repo()
