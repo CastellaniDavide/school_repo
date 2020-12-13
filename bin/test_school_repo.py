5c220442be835fedc8b9de6a88ce0b9105f75e88
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 13:29:49 +0000

    v03.01 try #7

--- bin/test_school_repo.py
+++ bin/test_school_repo.py
@@ -7,10 +7,10 @@ def test_school_repo_():
     """My tester
     """
     # Change part of file
+    assert(argv[1] != "${{ secrets.TOCKEN }}")
     modified = open(path.join('.', 'bin', 'school_repo.py'), 'r+').read().replace('TOKEN = "TODO"', f'TOCKEN = "{argv[1]}"').replace('ORGANIZATION = \"TODO\"', f'ORGANIZATION = \"CastellaniDavideTest\"').replace('END_OF_ORGANIZATION_EMAIL = \"TODO\"', f'END_OF_ORGANIZATION_EMAIL = \"\"').replace('INITIAL_PART_OF_REPOS = \"TODO\"', f'INITIAL_PART_OF_REPOS = \"{int(datetime.now().timestamp())}\"')
     open(path.join('.', 'bin', 'school_repo.py'), 'w+').write(modified)
-    print(argv[1])
-
+    
     # Try code
     school_repo()
 
