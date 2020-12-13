cf07c77a4daa4df9aff30db5717b697758615bcb
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 13:24:31 +0000

    v03.01 try #5

---
+++ bin/test_school_repo.py
@@ -0,0 +1,17 @@
+from os import path
+from datetime import datetime
+from school_repo import school_repo
+from sys import argv
+
+def test_school_repo_():
+    """My tester
+    """
+    # Change part of file
+    modified = open(path.join('.', 'bin', 'school_repo.py'), 'r+').read().replace('TOKEN = "TODO"', f'TOCKEN = "{argv[1]}"').replace('ORGANIZATION = \"TODO\"', f'ORGANIZATION = \"CastellaniDavideTest\"').replace('END_OF_ORGANIZATION_EMAIL = \"TODO\"', f'END_OF_ORGANIZATION_EMAIL = \"\"').replace('INITIAL_PART_OF_REPOS = \"TODO\"', f'INITIAL_PART_OF_REPOS = \"{int(datetime.now().timestamp())}\"')
+    open(path.join('.', 'bin', 'school_repo.py'), 'w+').write(modified)
+
+    # Try code
+    school_repo()
+
+if __name__ == "__main__":
+    test_school_repo_()
