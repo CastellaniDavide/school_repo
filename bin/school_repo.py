5f484b735f26e0ecf035197f3c0c0dbe7da6617a
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-13 10:09:41 +0000

    try multithreading -> unstable

--- bin/school_repo.py
+++ bin/school_repo.py
@@ -1,4 +1,5 @@
 """school_repo
+by Castellani Davide
 """
 from github import Github
 from collections import defaultdict
@@ -6,9 +7,10 @@ from os import path
 from datetime import datetime
 from time import sleep
 from requests import get
+from threading import Thread, active_count
 
 __author__ = "help@castellanidavide.it"
-__version__ = "02.01 2020-12-08"
+__version__ = "03.01 2020-12-13"
 
 TOKEN = "TODO"
 ORGANIZATION = "TODO"
@@ -26,7 +28,9 @@ class school_repo:
 		self.debug = debug
 
 		# Open log file
-		self.log = open(path.join(".", "log", "trace.log"), "a+")
+		self.log = open(path.join(".", "log", f"trace_{int(self.start.timestamp())}.log"), "a+")
+		self.log.write("Execution_code,Repo,Message,time")
+			
 		self.print(f"Start: {self.start}")
 		self.read_input()
 
@@ -37,12 +41,14 @@ class school_repo:
 		self.get_tempate()
 
 		# Create repos and brances
-		self.create_repos()
+		Thread(target = self.create_repos).start()
 
 		# Add teachers to the organization
-		self.add_teachers()
+		Thread(target = self.add_teachers).start()
 
-		self.print(f"Ended: {datetime.now()}\nTotal time: {datetime.now() - self.start}\n")
+		while (active_count() != 1) : print(active_count()) # Wait the end
+		self.print(f"Ended: {datetime.now()}")
+		self.print(f"Total time: {datetime.now() - self.start}\n")
 		self.log.close()
 	
 	def login(self):
@@ -55,7 +61,7 @@ class school_repo:
 		"""Read the input file and transform it into a map
 		"""
 		self.input = defaultdict(list)
-		for line in school_repo.csv2array(open(path.join(".", "flussi", "students.csv"), "r+").read())[1:-1]:
+		for line in self.csv2array(open(path.join(".", "flussi", "students.csv"), "r+").read())[1:-1]:
 			self.input[line[1]].append(line[0])
 
 		self.teachers = []
@@ -64,7 +70,7 @@ class school_repo:
 
 		self.print("Input file readed")
 
-	def csv2array(csv):
+	def csv2array(self, csv):
 		"""Converts csv file to a py array
 		"""
 		array = []
@@ -81,7 +87,7 @@ class school_repo:
 		"""Create all repos and brances
 		"""
 		for repo_base_name in self.input:
-			self.create_repo(repo_base_name)
+			Thread(target = self.create_repo, args=(repo_base_name,)).start()
 			
 	def create_repo(self, repo_base_name):
 		""" Create a single repository with brances
@@ -91,40 +97,60 @@ class school_repo:
 			if repo == "":
 				try:
 					repo = self.g.get_repo(self.get_repo_name(repo_base_name, i))
-					self.print(f"Get an old repo: {self.get_repo_name(repo_base_name, i)}")
+					self.print(f"Get an old repo: {self.get_repo_name(repo_base_name, i)}", repo=repo)
 				except:
 					pass
 
 		if repo == "":
 			try:
 				repo = self.g.create_repo(self.get_repo_name(repo_base_name))
-				self.print(f"- Created repo for {repo_base_name} class, named {self.get_repo_name(repo_base_name)}")
+				self.print(f"Created repo for {repo_base_name} class, named {self.get_repo_name(repo_base_name)}", repo=repo)
 			except:
 				repo = self.g.get_repo(self.get_repo_name(repo_base_name))
-				self.print(f"- Get access to the repo for {repo_base_name} class, named {self.get_repo_name(repo_base_name)}")
+				self.print(f"- Get access to the repo for {repo_base_name} class, named {self.get_repo_name(repo_base_name)}", repo=repo)
 
 		try:
 			repo.create_file("README.md", "Created initial file", self.template)
-			self.print("\t- Created welcome file")
+			self.print("\t- Created welcome file", repo=repo)
 		except:
-			self.print("\t- Welcome file already exists")
-		
+			self.print("\t- Welcome file already exists", repo=repo)
+
 		main = self.get_main(repo)
 		main.edit_protection(user_push_restrictions=[])
 
 		for branch in self.input[repo_base_name]:
-			try:
-				repo.create_git_ref(ref=f'refs/heads/{branch}', sha=main.commit.sha)
-				self.print(f"\t- Created branch user: {branch}")
-			except:
-				self.print(f"\t- Branch already exists: {branch}")
+			Thread(target=self.branch, args=(repo, main, branch))
 
-			try:
-				self.g.invite_user(email=f"{branch}{END_OF_ORGANIZATION_EMAIL}", role="direct_member")
-				self.print(f"\t- Invited new user: {branch}{END_OF_ORGANIZATION_EMAIL}")
-			except:
-				self.print(f"\t- The user is in org or the email is not correct: {branch}{END_OF_ORGANIZATION_EMAIL}")
-				
+	def branch(self, repo, main, branch):
+		""" Understand if branch exist and if not create it and add user
+		"""
+		try:
+			repo.git.checkout(branch)
+			self.print(f"\t- Branch and User already exists: {branch}", repo=repo)
+		except repo.exc.GitCommandError:
+			Thread(target = self.my_create_branch, args=(repo, branch, main)).start()
+			Thread(target = self.add_student, args=(repo, branch, main)).start()
+
+	def my_create_branch(self, repo, branch, main):
+		"""Try to create the branch
+		"""		
+		try:
+			repo.create_git_ref(ref=f'refs/heads/{branch}', sha=main.commit.sha)
+			self.print(f"\t- Created branch user: {branch}", repo=repo)
+		except:
+			sleep(1) # Wait a moment
+			self.my_create_branch(repo, branch, main)
+
+	def add_student(self, repo, branch, main):
+		"""Try to add a new student to the repo
+		"""
+		try:
+			self.g.invite_user(email=f"{branch}{END_OF_ORGANIZATION_EMAIL}", role="direct_member")
+			self.print(f"\t- Invited new user: {branch}{END_OF_ORGANIZATION_EMAIL}", repo=repo)
+		except:
+			sleep(1) # Wait a moment
+			self.add_student(repo, branch, main)
+
 	def add_teachers(self):
 		"""Add teachers to the repo
 		"""
@@ -140,11 +166,11 @@ class school_repo:
 		"""
 		return f"{INITIAL_PART_OF_REPOS}{self.start.year - last - (1 if self.start.month < 7 else 0)}-{self.start.year + (3 if int(repo_base_name[0]) in [1, 2] else 6) - int(repo_base_name[0]) + (1 if self.start.month < 7 else 0)}-{'B' if int(repo_base_name[0]) in [1, 2] else 'T'}-{repo_base_name[1:]}"
 
-	def print(self, message):
+	def print(self, message, repo="No repo"):
 		"""Print wanted message
 		"""
-		self.log.write(f"{message}\n")
-		if self.debug : print(message)
+		if self.debug : print(f""""{self.start.timestamp()}","{str(repo).replace('Repository(full_name="', '').replace('")', '')}","{message}","{datetime.now().timestamp()}" """)
+		self.log.write(f""""{self.start.timestamp()}","{str(repo).replace('Repository(full_name="', '').replace('")', '')}","{message}","{datetime.now().timestamp()}"\n""")
 
 	def get_main(self, repo):
 		"""Returns main
@@ -153,8 +179,8 @@ class school_repo:
 			return repo.get_branch("main")
 		except:
 			sleep(1)	# Wait a while
-			self.print("\tI'm waiting and searching main branch")
-			return self.get_main()
+			self.print("I'm waiting and searching main branch", repo=repo)
+			return self.get_main(repo)
 
 	def get_tempate(self):
 		"""Gets template
